386cc30f1f79a15d902cb30cb288e0071c429edf
agdrall <kaaron999@gmail.com>
2018-06-28 17:11:05 -0700

    Added home page, base.html

--- thing/user_accounts/urls.py
+++ thing/user_accounts/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from . import views #import the view file in the current app
-
+from django.contrib.auth.views import login # login library
 
 urlpatterns = [
 	url(r'^$', views.home),
         #since these urls belong to the "user_accounts" app, they by default extend from www.asd/user_accounts/...
-        #also, at url /user_accounts/ , it will load the "home" object in the views file in this app. 
+        #also, at url /user_accounts/ , it will load the "home" object in the views file in this app.
+        url(r'login/$', login, {'template_name': 'user_accounts/login.html'})
+        #at url .../login, we load "login", with the template login.html
 ]
+
 
--- thing/user_accounts/views.py
+++ thing/user_accounts/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 # function based view
 def home(request):
-    return render(request, 'user_accounts/login.html') #this implicitely loads the '/template/user_accounts/login.html'
+    # you would usually pull these variables from a database. 
+    numbers = {1,2,3,4}
+    name = "ASD DDD"
+
+
+    # this implicitely loads the '/template/user_accounts/login.html'
+    # you can also pass in a variable list of information.
+    args = {'name': name, 'numbers': numbers}
+    return render(request, 'user_accounts/home.html', args) 
